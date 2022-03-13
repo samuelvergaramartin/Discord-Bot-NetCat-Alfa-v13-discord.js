fdf4a0721b98574b6447b15169dd5b791d56e96f
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-03-13 23:31:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 require("dotenv").config();
 const discord = require("discord.js");
+const fs = require('fs');
+const botprefix = require('./defaultprefix.json');
+var prefix = botprefix.prefix;
 const client = new discord.Client({
     intents: 98303
 });
+
+client.comandos = new discord.Collection();
+
+let archivos = fs.readdirSync('./commands').filter((f) => f.endsWith('.js'))
+
+for(var archi of archivos) {
+    let comando = require("./commands/" + archi)
+    client.comandos.set(comando.name, comando)
+}
 
 client.once("ready", (bot) => {
     client.user.setPresence({activities: [{name: 'Bot exclusivo del servidor oficial de NetCat | Disponible muy pronto', type: "PLAYING"}], status: "idle"});
     console.log(`Bot: ${bot.user.username}\nStatus: ${bot.presence.status}`);
 })
-//
+
+client.on("messageCreate", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+    if(!message.channel.permissionsFor(client.user).has('SEND_MESSAGES')) return message.author.send("Error, no tengo permisos suficientes.").catch(()=>{ return;});
+
+    const args = message.content.slice(prefix.length).trim().split(" ");
+    const command = args.shift().toLowerCase();
+    
+    
+     
+     if(!message.content.startsWith(prefix)) return;
+ let cmd = client.comandos.get(command)
+    if(cmd) {
+        return cmd.run(client, message, args)
+    }
+})
+
 client.login(process.env.TOKEN);
