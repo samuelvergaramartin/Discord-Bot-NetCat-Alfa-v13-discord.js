27ba7046e1b3b5ae1d752952c3d46251d50e8047
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-03-14 22:23:55 +0100

    Testing test.js

--- commands/test.js
+++ commands/test.js
@@ -3,7 +3,8 @@ var ncomando = "test";
 module.exports = {
     name: `${ncomando}`,
     run: async (client, message, args) => {
-        message.channel.send("Todo en orden.");
+        //message.channel.send("Todo en orden.");
+        message.channel.send(`Leyendo el valor de $client.username: ${client.username}`);
     }
 }
 console.log(`Comando: ${ncomando}: OK`.green)
