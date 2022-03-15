d35342b1b9c8e32b3e72ce734c1c4e31ee8503f9
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-03-15 11:49:21 +0100

    Correcciones del código del status presence

--- commands/test.js
+++ commands/test.js
@@ -1,17 +1,32 @@
 var colors = require('colors');
 var ncomando = "test";
+const db = require('megadb`)
+const estado = new db.crearDB('estadopersonalizado')
+var numerocero = 0;
 module.exports = {
     name: `${ncomando}`,
     run: async (client, message, args, discord) => {
         //message.channel.send("Todo en orden.");
-        /*let sv = client.guilds.cache.get("900856291461824562")
+        let sv = client.guilds.cache.get("900856291461824562")
         let x = sv.members.cache.get("911352430963347537")
-        message.channel.send(`Leyendo el valor de la variable declarada 'x': ${x}`)
-        message.channel.send(`Leyendo el valor de $x.presence: ${x.presence.status}`)
+        let estadopersonalizado = `${x.presence.status}`;
+        const suestado = await estado.obtener(`${x.id}`)
+        if(!suestado) {
+            estado.set(`${x.id}`, numerocero)
+            if(estadopersonalizado == `offline`) return;
+            if(!estadopersonalizado == `offline`) {
+                if(suestado == `{numerocero}`) return;
+                if(suestado == 1) {
+                    estado.restar(`${x.id}`, 1) //Me he dado cuenta del problema, no me cojía lo de sumar o restar 1 porque 1 o 0 son booleanos, tengo que declarar los valores de suma o resta como booleanos, o bien cambiarlos por un 2 o algo así. 1= TRUE, 0 = FALSE
+                }
+            }
+        }
+       /* message.channel.send(`Leyendo el valor de la variable declarada 'x': ${x}`)
+        message.channel.send(`Leyendo el valor de $x.presence: ${x.presence.status}`)*/
         //message.channel.send(`Leyendo el valor de $client.username: ${client.username}`);*/
-        /*setInterval(() => {
-            message.channel.send("Contador: 5 segundos + repetir, blucle infinito....")
-        }, 5000)*/
+        setInterval(() => {
+            
+        }, 5000)
     }
 }
 console.log(`Comando: ${ncomando}: OK`.green)
