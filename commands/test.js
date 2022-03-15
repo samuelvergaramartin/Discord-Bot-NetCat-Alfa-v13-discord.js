35d7de60e23c2c8d5ee0361d933cbe3d3b83faf7
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-03-15 13:47:13 +0100

    Fixed test.js

--- commands/test.js
+++ commands/test.js
@@ -2,7 +2,9 @@ var colors = require('colors');
 var ncomando = "test";
 const db = require('megadb`)
 const estado = new db.crearDB('estadopersonalizado')
-var estadooff = 2;
+const estadooff = 2;
+const estadoon = 4;
+const cambiarestado = 2;
 module.exports = {
     name: `${ncomando}`,
     run: async (client, message, args, discord) => {
@@ -11,22 +13,42 @@ module.exports = {
         let x = sv.members.cache.get("911352430963347537")
         let estadopersonalizado = `${x.presence.status}`;
         const suestado = await estado.obtener(`${x.id}`)
-        if(!suestado) {
-            estado.set(`${x.id}`, estadooff)
-            if(estadopersonalizado == `offline`) return;
+        setInterval(() => {
+             if(!suestado) {
+            
+            if(estadopersonalizado == `offline`) {
+                estado.set(`${x.id}`, estadooff)
+                message.channel.send(`${x} está actualmente **OFFLINE**`);
+            }
             if(!estadopersonalizado == `offline`) {
-                if(suestado == `{numerocero}`) return;
-                if(suestado == 1) {
-                    estado.restar(`${x.id}`, 1) //Me he dado cuenta del problema, no me cojía lo de sumar o restar 1 porque 1 o 0 son booleanos, tengo que declarar los valores de suma o resta como booleanos, o bien cambiarlos por un 2 o algo así. 1= TRUE, 0 = FALSE
+                estado.set(`${x.id}`, estadoon)
+                message.channel.send(`${x} está actualmente **ONLINE**`);
+                //Me he dado cuenta del problema, no me cojía lo de sumar o restar 1 porque 1 o 0 son booleanos, tengo que declarar los valores de suma o resta como booleanos, o bien cambiarlos por un 2 o algo así. 1= TRUE, 0 = FALSE
+                }
+            }else{
+                if(!estadopersonalizado == `offline`) {
+                    if(suestado == `${estadooff}`) {
+                        estado.sumar(`${x.id}`, cambiarestado)
+                        message.channel.send(`${x} está actualmente **ONLINE**`);
+                    }
+                    if(suestado == `${estadoon}`) return;
+                }else {
+                    if(suestado == `${estadoon}`) {
+                        estado.restar(`${x.id}`, cambiarestado)
+                        message.channel.send(`${x} está actualmente **OFFLINE**`);
+                    }
                 }
             }
-        }
+        }, 5000)
+       
+        
+        
        /* message.channel.send(`Leyendo el valor de la variable declarada 'x': ${x}`)
         message.channel.send(`Leyendo el valor de $x.presence: ${x.presence.status}`)*/
         //message.channel.send(`Leyendo el valor de $client.username: ${client.username}`);*/
-        setInterval(() => {
+        /*setInterval(() => {
             
-        }, 5000)
+        }, 5000)*/
     }
 }
 console.log(`Comando: ${ncomando}: OK`.green)
