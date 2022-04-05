ef42127db658d6cc521df250150e436f1491b99b
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-04-05 11:25:20 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,14 @@
 module.exports = {
   name: "ping",
   run: async (client, message, args) => {
-    message.channel.send(`__**Latencia:**__ \nPing: ${client.ws.ping} ms`);
+    var t1 = Date.now();
+    message.channel.send("Midiendo latencia...")
+    message.channel.send("Calculando...").then((msg)=> {
+      var t2 = Date.now();
+      let restar = t1 - t2;
+      let latbot = Math.abs(restar);
+      msg.edit("**Tiempo de respuesta del bot**:" +latbot+" ms\n**Discord API**: "  +client.ws.ping+" ms")
+    })
+    
   }
 }
